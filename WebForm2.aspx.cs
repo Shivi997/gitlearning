24259a1033c8e0ebbcfc0b972a3a525e05619718
Shivi997 <114285128+Shivi997@users.noreply.github.com>
2023-05-16 10:13:27 +0530

    Add files via upload

---
+++ WebForm2.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebAssignment
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+      
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+           
+
+        }
+
+        protected void Button_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+
+            con.Open();
+            SqlCommand cmd = new SqlCommand("insert into dbbooks values('" + TextBox1.Text + "','" + TextBox2.Text + "','" + TextBox3.Text + "',' " + TextBox4.Text+"')", con);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            
+            Bookdetails.DataBind();
+            TextBox1.Text = "";
+            TextBox2.Text = "";
+            TextBox3.Text = "";
+            TextBox4.Text = "";
+            Label6.Text = "Data has been inserted";
+            Label5.Text = "success";
+
+        }
+
+        protected void Bookdetails_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
