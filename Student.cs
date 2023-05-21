56dfdc32ee721c4bf212be1bf235b7bba08891ae
Shivi997 <114285128+Shivi997@users.noreply.github.com>
2023-05-22 00:26:10 +0530

    Add files via upload

---
+++ Student.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MvcApplication3.Models
+{
+    [Table("Student")]
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+        public string StudentName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;  
+        public int age { get; set;}
+        public int mobileno { get; set; }
+
+        public class StudentContext : DbContext
+        {
+            public DbSet<Student> Stud { get; set; }
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                var strConnection = @"Data Source=W-674PY03-1;Initial Catalog=Shivam;Persist Security Info=True;User ID=sa;Password=Password@12345;TrustServerCertificate=True";
+                base.OnConfiguring(optionsBuilder);
+                optionsBuilder.UseSqlServer(strConnection);
+            }
+        }
+        
+    }
+   }
+
---
+++ StudentController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcApplication3.Models;
+using static MvcApplication3.Models.Student;
+
+namespace MvcApplication3.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            var context = new StudentContext();
+            var model = context.Stud.ToList();
+            return View(model);
+        }
+
+        public IActionResult OnAdd()
+        {
+            var context = new Student();
+            return View(context);
+        }
+        [HttpPost]
+        public IActionResult OnAdd(Student postedRec)
+        {
+            var model = new StudentContext();
+            model.Stud.Add(postedRec);
+            model.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult OnFind(int id)
+        {
+
+            var context = new StudentContext();
+            var model = context.Stud.FirstOrDefault(r => r.Id == id);
+            if (model == null)
+            {
+                throw new Exception("No record Found");
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult OnFind(Student postedData)
+        {
+            var context = new StudentContext();
+            var rec = context.Stud.FirstOrDefault(rec => rec.Id == postedData.Id);
+            if (rec != null)
+            {
+                rec.StudentName = postedData.StudentName;
+                rec.Email = postedData.Email;
+                rec.age = postedData.age;
+                rec.mobileno = postedData.mobileno;
+                context.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+            else
+            {
+                return View(postedData);
+            }
+          
+
+        }
+        public IActionResult OnDelete(int id)
+        {
+            var context = new StudentContext();
+            var model = context.Stud.FirstOrDefault(r=>r.Id == id);
+            if(model!=null)
+            {
+                context.Stud.Remove(model); 
+                context.SaveChanges();
+                return RedirectToAction("Index");   
+            }
+            else
+            {
+                throw new Exception("No Data Found");
+            }
+
+        }
+    }
+}
